f81e908fdd62ff5656c9cd4f4f9805d3b5a2cb07
Inioluwa Akinyosoye <inisoye@gmail.com>
2020-12-21 19:16:24 +0100

    Change endpoint

--- src/services/data.js
+++ src/services/data.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
-const baseUrl = 'http://localhost:3001/authenticatedData';
+const baseUrl = 'https://dtekluva.github.io/weather_app/authenticatedData.json';
 
 const getAuthenticated = () => {
   const request = axios.get(baseUrl);
-  return request.then((response) => response.data);
+  return request.then((response) => response.data.authenticatedData);
 };
 
 export default { getAuthenticated };
