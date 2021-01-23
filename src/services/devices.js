ab5370a9011a107b7f5f0ad63d0e22905dfda629
dtekluva <kboysreel@gmail.com>
2021-01-23 04:21:53 +0100

    switched to dynamic id

--- src/services/devices.js
+++ src/services/devices.js
@@ -49,8 +49,8 @@ const setEndpointDataTimeInterval = (newEndpointDataTimeInterval) => {
 
 const getAllData = async () => {
   // Add interval to url
-  // const baseUrl = `${baseUrlPrefix}/${userId}/${endpointDateRange}/${endpointDataTimeInterval}`;
-  const baseUrl = `${baseUrlPrefix}/2/${endpointDateRange}/${endpointDataTimeInterval}`;
+  const baseUrl = `${baseUrlPrefix}/${userId}/${endpointDateRange}/${endpointDataTimeInterval}`;
+  // const baseUrl = `${baseUrlPrefix}/2/${endpointDateRange}/${endpointDataTimeInterval}`;
 
   // console.log(baseUrl);
 
