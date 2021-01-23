8f585b630ddaf554e9406a3a17f4f4a596610f91
Inioluwa Akinyosoye <inisoye@gmail.com>
2021-01-23 19:12:40 +0100

    fix: replace hardcoded user ID with dynamically generated value

--- src/services/devices.js
+++ src/services/devices.js
@@ -49,10 +49,7 @@ const setEndpointDataTimeInterval = (newEndpointDataTimeInterval) => {
 
 const getAllData = async () => {
   // Add interval to url
-  // const baseUrl = `${baseUrlPrefix}/${userId}/${endpointDateRange}/${endpointDataTimeInterval}`;
-  const baseUrl = `${baseUrlPrefix}/2/${endpointDateRange}/${endpointDataTimeInterval}`;
-
-  // console.log(baseUrl);
+  const baseUrl = `${baseUrlPrefix}/${userId}/${endpointDateRange}/${endpointDataTimeInterval}`;
 
   const config = {
     headers: { Authorization: token },
