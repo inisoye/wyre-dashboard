811a89f6118649c86a2f4999f94befe0779594ca
idriskelani <idmega2000@gmail.com>
2021-10-13 16:37:18 +0100

    refactor env data

--- src/config/EnvData.js
+++ src/config/EnvData.js
@@ -1,7 +1,7 @@
 
 const EnvData = {
-    REACT_APP_UPLOAD_LIMIT: process.env.REACT_APP_UPLOAD_LIMIT || 'https://wyreng.xyz/api/v1/',
-    REACT_APP_API_URL: process.env.REACT_APP_API_URL || 10,
+    REACT_APP_UPLOAD_LIMIT: process.env.REACT_APP_UPLOAD_LIMIT  || 10,
+    REACT_APP_API_URL: process.env.REACT_APP_API_URL || 'https://wyreng.xyz/api/v1/'
 }
 
 export default EnvData;
