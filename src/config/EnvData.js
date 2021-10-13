60d4873b15564b18e35b0f8184ff985e0d2323d2
idriskelani <idmega2000@gmail.com>
2021-10-13 16:12:26 +0100

    chore(cost tracker): add default environment variables

---
+++ src/config/EnvData.js
@@ -0,0 +1,7 @@
+
+const EnvData = {
+    REACT_APP_UPLOAD_LIMIT: process.env.REACT_APP_UPLOAD_LIMIT || 'https://wyreng.xyz/api/v1/',
+    REACT_APP_API_URL: process.env.REACT_APP_API_URL || 10,
+}
+
+export default EnvData;
